2eed5a0750865e7be0419b08239d88cf3b3db9b7
Pushpak <130127557+mpushpak@users.noreply.github.com>
2024-10-21 13:46:12 +0000

    Converrsion

---
+++ basics/Conversion and operation.js
@@ -0,0 +1,7 @@
+let age = 33
+console.log(typeof age);
+console.log(typeof(age));
+
+//Change string into number (If we don't know)
+let valueInnumber = Number(age)
+console.log(typeof valueInnumber);
