4ec16c56be7decdebc62b7e1a5d19f26f4e32b7d
Pushpak <130127557+mpushpak@users.noreply.github.com>
2024-10-21 12:25:09 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+    "use strict"; // treat all js code as newer version
+
+    alert (3+3) // We use node js
+
+
+    //Primitive datatypes
+    //number- 2 to 53
+    //bigint - Ex.trading websites
+    //string - "Test"
+    //bollen - true/false
+    //null - yeh undefined nahi hai (it is a Object)
+    //undefined - isme kuch defien hi nahi kiya hai
+    //symbol - using React to create multiple components
+
+
+   
